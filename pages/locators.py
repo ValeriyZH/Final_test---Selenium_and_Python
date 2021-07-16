0902a479d717867995c8fd2823e55ff3d34d9a41
ValeriyZH <58189787+ValeriyZH@users.noreply.github.com>
2021-07-16 17:28:23 +0300

    Проверка перехода на страницу логина
    Добавлена проверка перехода на страницу логина и проверка наличия форм залогинивания и регистрации. Добавлен файл login_page, куда вынесены все селекторы для страницы логина

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators()
+    id_login = (By.CSS_SELECTOR, "#id_login-username")
+    id_passw = (By.CSS_SELECTOR, "#id_login-password")
+    id_form = (By.CSS_SELECTOR, "#login_form")
+    id_registr = (By.CSS_SELECTOR, "#register_form")
